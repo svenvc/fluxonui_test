c288c9276ad6c1cba0aefe6e15e9fe452fb24c96
Sven Van Caekenberghe <sven@stfx.eu>
2025-09-27 11:48:01 +0200

    mix phx.gen.auth Accounts User users --live

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,19 @@
 # General application configuration
 import Config
 
+config :fluxonui_test, :scopes,
+  user: [
+    default: true,
+    module: FluxonUITest.Accounts.Scope,
+    assign_key: :current_scope,
+    access_path: [:user, :id],
+    schema_key: :user_id,
+    schema_type: :id,
+    schema_table: :users,
+    test_data_fixture: FluxonUITest.AccountsFixtures,
+    test_setup_helper: :register_and_log_in_user
+  ]
+
 config :fluxonui_test,
   namespace: FluxonUITest,
   ecto_repos: [FluxonUITest.Repo],
--- lib/fluxonui_test_web/router.ex
+++ lib/fluxonui_test_web/router.ex
@@ -1,6 +1,8 @@
 defmodule FluxonUITestWeb.Router do
   use FluxonUITestWeb, :router
 
+  import FluxonUITestWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule FluxonUITestWeb.Router do
     plug :put_root_layout, html: {FluxonUITestWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_scope_for_user
   end
 
   pipeline :api do
@@ -18,11 +21,6 @@ defmodule FluxonUITestWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
-
-    live "/todos", TodoLive.Index, :index
-    live "/todos/new", TodoLive.Form, :new
-    live "/todos/:id", TodoLive.Show, :show
-    live "/todos/:id/edit", TodoLive.Form, :edit
   end
 
   # Other scopes may use custom stacks.
@@ -45,5 +43,37 @@ defmodule FluxonUITestWeb.Router do
       live_dashboard "/dashboard", metrics: FluxonUITestWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
+  end
+
+  ## Authentication routes
+
+  scope "/", FluxonUITestWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{FluxonUITestWeb.UserAuth, :require_authenticated}] do
+      live "/todos", TodoLive.Index, :index
+      live "/todos/new", TodoLive.Form, :new
+      live "/todos/:id", TodoLive.Show, :show
+      live "/todos/:id/edit", TodoLive.Form, :edit
+      live "/users/settings", UserLive.Settings, :edit
+      live "/users/settings/confirm-email/:token", UserLive.Settings, :confirm_email
+    end
+
+    post "/users/update-password", UserSessionController, :update_password
+  end
+
+  scope "/", FluxonUITestWeb do
+    pipe_through [:browser]
+
+    live_session :current_user,
+      on_mount: [{FluxonUITestWeb.UserAuth, :mount_current_scope}] do
+      live "/users/register", UserLive.Registration, :new
+      live "/users/log-in", UserLive.Login, :new
+      live "/users/log-in/:token", UserLive.Confirmation, :new
+    end
+
+    post "/users/log-in", UserSessionController, :create
+    delete "/users/log-out", UserSessionController, :delete
   end
 end
